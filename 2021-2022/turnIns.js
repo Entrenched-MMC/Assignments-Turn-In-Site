2067e9123ca3c3206154cc4ddd5591461ab08ac7
Entrenched-MMC <101645632+Entrenched-MMC@users.noreply.github.com>
2022-04-08 11:04:51 -0400

    Update turnIns.js

--- 2021-2022/turnIns.js
+++ 2021-2022/turnIns.js
@@ -1,7 +1,14 @@
 let turnIn = false;
 
-function turnIn("class", "assignmentName", "date", "dueDate") {
-  if(turnIn)
+function turnIn("className", "assignmentName", "Date", "dueDate") {
+  if(turnIn == true) {
+    let classroom = className;
+    let assignment = assignmentName;
+    let date = Date;
+    let due = dueDate;
+    let assignmentInfo = [classroom, assignment, date, due];
+    saveAs(assignmentInfo, "assignment.txt")
+  }
 }
 
 function write("class", "link", "date", "dueDate") {}
